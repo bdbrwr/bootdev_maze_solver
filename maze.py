61f981155978be486eeff74fb9358f1e3a436e17
bdbrwr <bart@bdbrwr.nl>
2025-05-07 00:20:01 +0200

    Break entrance and exit (no walls)

--- maze.py
+++ maze.py
@@ -13,6 +13,7 @@ class Maze():
         self._win = win
 
         self._create_cells()
+        self._break_entrance_and_exit()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -39,5 +40,9 @@ class Maze():
             return        
         self._win.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._cells[self._num_cols][self._num_rows].has_bottom_wall = False
 
         
