2060586d563d6a919ddf1abb070a29d923687cb8
bdbrwr <bart@bdbrwr.nl>
2025-05-06 23:30:45 +0200

    Cell draw move

--- cell.py
+++ cell.py
@@ -26,3 +26,17 @@ class Cell():
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
 
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            line_color = "red"
+        else:
+            line_color = "grey"
+        
+        from_x = (self._x1 + self._x2) / 2
+        from_y = (self._y1 + self._y2) / 2
+        to_x = (to_cell._x1 + to_cell._x2) / 2
+        to_y = (to_cell._y1 + to_cell._y2) / 2
+
+        line = Line(Point(from_x, from_y), Point(to_x, to_y))
+        self._win.draw_line(line, fill_color=line_color)
+
