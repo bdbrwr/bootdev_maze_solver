a539c26a832553d80cd0e41f16acad7264ab5ad6
bdbrwr <bart@bdbrwr.nl>
2025-05-06 23:16:50 +0200

    Cell class

--- graphics.py
+++ graphics.py
@@ -25,6 +25,9 @@ class Window():
     def draw_line(self, line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
 
+    def draw_cell(self, cell, fill_color="black"):
+        cell.draw(self.__canvas, fill_color)
+
 
 class Point():
     def __init__(self, x, y):
@@ -37,6 +40,28 @@ class Line():
         self.point_1 = point_1
         self.point_2 = point_2
 
-
     def draw(self, canvas, fill_color="black"):
         canvas.create_line(self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill=fill_color, width =2)
+
+
+class Cell():
+    def __init__(self, x1, y1, x2, y2):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = x1
+        self.__x2 = x2
+        self.__y1 = y1
+        self.__y2 = y2
+
+    def draw(self, canvas, fill_color="black"):
+        if self.has_left_wall:
+            canvas.create_line(self.__x1, self.__y1, self.__x1, self.__y2, fill = fill_color, width = 1)
+        if self.has_right_wall:
+            canvas.create_line(self.__x2, self.__y1, self.__x2, self.__y2, fill = fill_color, width = 1)
+        if self.has_top_wall:
+            canvas.create_line(self.__x1, self.__y1, self.__x2, self.__y1, fill = fill_color, width = 1)
+        if self.has_bottom_wall:
+            canvas.create_line(self.__x1, self.__y2, self.__x2, self.__y2, fill = fill_color, width = 1)
+
--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-from graphics import Window, Point, Line
+from graphics import Window, Point, Line, Cell
 
 
 def main():
     win = Window(800, 600)
 
-    p1 = Point(100,200)
-    p2 = Point(100,400)
-    p3 = Point(200,200)
-    p4 = Point(200,400)
+    c1 = Cell(200,200,400,400)
+    c1.has_bottom_wall = False
+    c2 = Cell(100,100,150,150)
+    c3 = Cell(500,500,600,700)
+    c3.has_top_wall = False
 
-    l1 = Line(p1, p2)
-    l2 = Line(p3, p4)
-    l3 = Line(p1, p3)
-
-    win.draw_line(l1, "black")
-    win.draw_line(l2, "red")
-    win.draw_line(l3, "blue")
+    win.draw_cell(c1)
+    win.draw_cell(c2)
+    win.draw_cell(c3)
 
     win.wait_for_close()
 
